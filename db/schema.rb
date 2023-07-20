c64a2c750a985b52c1f5bc2bb86e6c5157b083c8
Abu Masum Didar <masumdidar.cse@gmail.com>
2023-07-20 15:51:19 +0600

    sales count to product

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_19_103644) do
+ActiveRecord::Schema.define(version: 2023_07_20_094439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2023_07_19_103644) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "sales_count", default: 0, null: false
   end
 
 end
